e277ee522d456ecf23c9341e5d3d23a8f41d26cd
Edgardo Aznar <edgardoaznar@gmail.com>
2017-07-05 11:00:47 -0400

    Added second factorial function that uses recursion instead of a loop

--- Factorial.py
+++ Factorial.py
@@ -1,6 +1,8 @@
 """
-A simple function to calculate the factorial of a number
+A simple function to calculate the factorial of a integer
 """
+
+#This first version just uses a simple while loop
 
 def factorial(n):
     num = 1
@@ -9,4 +11,14 @@ def factorial(n):
         n -= 1
     return num
     
-print factorial(500)
+print factorial(10)
+
+#This version uses recursion
+
+def recfac(n):
+    if n == 1 or n == 0: 
+        return 1
+    else:
+        return n * recfac(n-1)
+
+print recfac(10)
