ff8286df20aa0fdc343a350665f583608049beb7
Edd Aznar <edgardoaznar@gmail.com>
2018-01-25 13:57:16 -0500

    Added new method to SingleLinkedList class

--- Linked_List.py
+++ Linked_List.py
@@ -13,7 +13,6 @@ class ListNode:
 
     def has_value(self, value):
         #This method comapares the value with the node data 
-        
         if self.data == value:
             return True
         else:
@@ -27,30 +26,39 @@ class SingleLinkedList:
     
     def add_list_item(self, item):
         #This adds an item at the end of the list 
-        
         if not isinstance(item, ListNode):
             item = ListNode(item)
         if self.head is None:
             self.head = item
         else:
-            self.tail.next = item
-            
+            self.tail.next = item    
+        
         self.tail = item
         
         return
     
     def list_length(self):
         #This returns the number of list items
-        
         count = 0
         current_node = self.head
         
         while current_node is not None:
             count = count + 1
-            #Jump to linked node 
-            current_node = current_node.next
-            
+            #Jump to next node 
+            current_node = current_node.next    
+        
         return count
+    
+    def output_list(self):
+        #This outputs the list 
+        current_node = self.head
+        
+        while current_node is not None:
+            print(current_node.data)
+            current_node = current_node.next
+        
+        return
+    
     
 node1 = ListNode(15) 
 node2 = ListNode(8.2)
@@ -60,7 +68,10 @@ node4 = ListNode(15)
 track = SingleLinkedList()
 print("track length: %i" % track.list_length())
 
-
+for current_item in [node1, node2, item3, node4]:
+    track.add_list_item(current_item)
+    print("track length: %i" % track.list_length())
+    track.output_list()
 
 
 
