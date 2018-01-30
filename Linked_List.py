705f33f7134213f1d205a1193a5a8be20e2bbca1
Edd Aznar <edgardoaznar@gmail.com>
2018-01-30 12:45:45 -0500

    Added Double Linked List Class

--- Linked_List.py
+++ Linked_List.py
@@ -8,6 +8,8 @@ class ListNode:
         #This stores data     
         self.next = None
         #This references the next item 
+        self.previous = None
+        #This referneces the previous item 
         return
 
     def has_value(self, value):
@@ -102,7 +104,7 @@ class SingleLinkedList:
             
         return
     
-    
+"""    
 node1 = ListNode(15) 
 node2 = ListNode(8.2)
 item3 = "Berlin"
@@ -115,8 +117,121 @@ for current_item in [node1, node2, item3, node4]:
     track.add_list_item(current_item)
     print("track length: %i" % track.list_length())
     track.output_list()
+"""
 
+class DoubleLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None 
+        return 
+    
+    def list_length(self):
+        #returns the length of the list 
+        count = 0
+        current_node = self.head
+        
+        while current_node is not None:
+            count = count + 1 
+            current_node = current_node.next
+            
+        return count
+    
+    def output_list(self):
+        #This outputs the value of the node 
+        current_node = self.head
+        
+        while current_node is not None:
+            print(current_node.data)
+            current_node = current_node.next 
+            
+        return 
+    
+    def unordered_search(self, value):
+        #This searches the list for the node that has the value that is given
+        current_node = self.head
+        node_id = 1
+        results = []
+        
+        while current_node is not None:
+            if current_node.has_value(value):
+                results.append(node_id)
+                
+            current_node = current_node.next
+            node_id = node_id + 1
+            #This jumps to the linked node 
+            
+        return results
+    
+    def add_list_item(self, item):
+        #This adds an item at the end of the lsit 
+        
+        if isinstance(item, ListNode):
+            if self.head is None:
+                self.head = item
+                item.previous is None 
+                item.next = None
+                self.tail = item 
+            else: 
+                self.tail.next = item 
+                item.previous = self.tail
+                self.tail = item 
+                
+            return
+        
+    def remove_list_item_by_id(self, item_id):
+        #Removes the item with the item id 
+        
+        current_id = 1
+        current_node = self.head
+        
+        while current_node is not None:
+            previous_node = current_node.previous
+            next_node = current_node.next
+            
+            if current_id == item_id:
+                #This is if this is the first node
+                if previous_node is not None:
+                    previous_node.next = next_node 
+                    if next_node is not None:
+                        next_node.previous = previous_node
+                else:
+                    self.head = next_node
+                    if next_node is not None:
+                        next_node.previous = None
+                #Here we don't have to look any further
+                
+                return
+            
+            current_node = next_node
+            current_id = current_id + 1 
+            
+        return
+    
+node1 = ListNode(15)  
+node2 = ListNode(8.2)  
+node3 = ListNode("Berlin")  
+node4 = ListNode(15)
 
+track = DoubleLinkedList()  
+print("track length: %i" % track.list_length())
+
+for current_node in [node1, node2, node3, node4]:  
+    track.add_list_item(current_node)
+    print("track length: %i" % track.list_length())
+    track.output_list()
+
+results = track.unordered_search(15)  
+print(results)
+
+track.remove_list_item_by_id(4)  
+track.output_list()    
+    
+    
+                        
+                        
+        
+        
+            
 
 
  
@@ -124,8 +239,8 @@ for current_item in [node1, node2, item3, node4]:
     
     
     
-    
-    
-    
-    
+
+
+
+
     
